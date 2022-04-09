e9871c32c01285efdead2094a060ed35c2ce8a52
Yunjoo Son <thsduswn4141@naver.com>
2022-04-09 18:08:46 +0900

    #4 [modify] 2차 세미나 실천과제 - 주석 풀기

--- Seminar2/async.ts
+++ Seminar2/async.ts
@@ -1,91 +1,91 @@
-// console.log("Hello Server");
+console.log("Hello Server");
 
-// // 비동기 처리 방식 - Callback Function
-// setTimeout((): void => {
-//     console.log("Hello Server 2");
-// }, 3000);
+// 비동기 처리 방식 - Callback Function
+setTimeout((): void => {
+    console.log("Hello Server 2");
+}, 3000);
 
-// console.log("Hello Server 3");
-
-
-// // 비동기 처리 방식 - Promise
-// const condition: boolean = true;
-// //const condition: boolean = false;
-
-// const promise = new Promise((resolve, reject) => {
-//     if (condition) {
-//         resolve("성공");
-//     } else {
-//         reject(new Error("에러 발생! condition false"));
-//     }
-// });
-
-// promise
-//     .then((resolveData): void => {
-//         console.log(resolveData + "이다~!!!")
-//     })
-//     .catch(err => console.log(err));
+console.log("Hello Server 3");
 
 
-// // Promise Chaining
-// const restaurant = (callback: () => void, time: number) => {
-//     setTimeout(callback, time);
-// }
+// 비동기 처리 방식 - Promise
+const condition: boolean = true;
+//const condition: boolean = false;
 
-// const order = (): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//         restaurant(() => {
-//             console.log("[레스토랑 진행 상황 - 음식 주문]");
-//             resolve("음식 주문 시작"); // resolve가 되면 string을 반환하기 때문에 order의 반환형도 Promise<string>
-//         }, 1000);
-//     });
-// }
+const promise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve("성공");
+    } else {
+        reject(new Error("에러 발생! condition false"));
+    }
+});
 
-// const cook = (progress: string): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//         restaurant(() => {
-//             console.log("[레스토랑 진행 상황 - 음식 조리]");
-//             resolve(`${progress} -> 음식 조리 중`);
-//         }, 2000);
-//     });
-// }
+promise
+    .then((resolveData): void => {
+        console.log(resolveData + "이다~!!!")
+    })
+    .catch(err => console.log(err));
 
-// const serving = (progress: string): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//         restaurant(() => {
-//             console.log("[레스토랑 진행 상황 - 음식 서빙]");
-//             resolve(`${progress} -> 음식 서빙 중`);
-//         }, 3000);
-//     });
-// }
 
-// const eat = (progress: string): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//         restaurant(() => {
-//             console.log("[레스토랑 진행 상황 - 음식 먹기]");
-//             resolve(`${progress} -> 음식 먹는 중`);
-//         }, 4000);
-//     });
-// }
+// Promise Chaining
+const restaurant = (callback: () => void, time: number) => {
+    setTimeout(callback, time);
+}
 
-// order()
-//     .then(progress => cook(progress))
-//     .then(progress => serving(progress))
-//     .then(progress => eat(progress))
-//     .then(progress => console.log(progress))
+const order = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        restaurant(() => {
+            console.log("[레스토랑 진행 상황 - 음식 주문]");
+            resolve("음식 주문 시작"); // resolve가 되면 string을 반환하기 때문에 order의 반환형도 Promise<string>
+        }, 1000);
+    });
+}
 
-// Promise.resolve(123)
-//     .then(res => {
-//         throw new Error("에러 발생!");
-//         return 456; // 절대 실행 되지 않음!! 위에 에러 발생했기 때문에 catch로 넘어감!
-//     })
-//     .then(res => {
-//         console.log(res); // 절대 실행 되지 않음!! 위에 에러 발생했기 때문에 catch로 넘어감!
-//         return Promise.resolve(789);
-//     })
-//     .catch(err => {
-//         console.log(err.message)
-//     });
+const cook = (progress: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        restaurant(() => {
+            console.log("[레스토랑 진행 상황 - 음식 조리]");
+            resolve(`${progress} -> 음식 조리 중`);
+        }, 2000);
+    });
+}
+
+const serving = (progress: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        restaurant(() => {
+            console.log("[레스토랑 진행 상황 - 음식 서빙]");
+            resolve(`${progress} -> 음식 서빙 중`);
+        }, 3000);
+    });
+}
+
+const eat = (progress: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        restaurant(() => {
+            console.log("[레스토랑 진행 상황 - 음식 먹기]");
+            resolve(`${progress} -> 음식 먹는 중`);
+        }, 4000);
+    });
+}
+
+order()
+    .then(progress => cook(progress))
+    .then(progress => serving(progress))
+    .then(progress => eat(progress))
+    .then(progress => console.log(progress))
+
+Promise.resolve(123)
+    .then(res => {
+        throw new Error("에러 발생!");
+        return 456; // 절대 실행 되지 않음!! 위에 에러 발생했기 때문에 catch로 넘어감!
+    })
+    .then(res => {
+        console.log(res); // 절대 실행 되지 않음!! 위에 에러 발생했기 때문에 catch로 넘어감!
+        return Promise.resolve(789);
+    })
+    .catch(err => {
+        console.log(err.message)
+    });
 
 
 // 비동기 처리 방식 - async/await
